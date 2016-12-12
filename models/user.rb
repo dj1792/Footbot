cb1ca9c1ef0e7f91f606101fc6bddeaaff89e385
dj1792 <djayaswa@andrew.cmu.edu>
2016-12-12 12:22:37 -0500

    sekfsddf

--- models/user.rb
+++ models/user.rb
@@ -1,8 +1,8 @@
  class User < ActiveRecord::Base
-	validates_presence_of :name
+	#validates_presence_of :name
     validates_presence_of :phone_no
-    validates_presence_of :notify_before_match
-    validates_presence_of :get_live_updates
+    #validates_presence_of :notify_before_match
+    #validates_presence_of :get_live_updates
 
     has_many :preferences
 end
