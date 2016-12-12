d99d7d1564f90df7220be1091aa31a2a9ae2a2e6
dj1792 <djayaswa@andrew.cmu.edu>
2016-12-12 12:26:06 -0500

    cmon

--- models/preference.rb
+++ models/preference.rb
@@ -1,7 +1,7 @@
  class Preference < ActiveRecord::Base
-	validates_presence_of :user_id
-    validates_presence_of :league_id
-    validates_presence_of :team_name
+	# validates_presence_of :user_id
+ #    validates_presence_of :league_id
+ #    validates_presence_of :team_name
 
     has_many :teamdetails
     belongs_to :users
